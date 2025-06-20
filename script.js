c33fa2f8b3592b0bd58b0edf795c5d87b2bfcd7c
Tandoori-Nights <tandoorinights1997@gmail.com>
2025-06-21 00:28:39 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-document.getElementById('logo').addEventListener('click', () => {
-  const fire = document.getElementById('logo-fire');
-  fire.style.display = 'block';
-  setTimeout(() => {
-    fire.style.display = 'none';
-  }, 1000);
-});
